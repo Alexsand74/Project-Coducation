0021d7833355f7bd6fc0de794f9081fe4c8daf69
Alexsand74 <1974alexsand@gmail.com>
2023-01-03 01:02:51 +0300

    The code is in the !
    Код находится в ветке !

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("The code is in the !");
         System.out.println("Код находится в ветке !");
+        System.out.println("Код находится в ветке !");
     }
 }
