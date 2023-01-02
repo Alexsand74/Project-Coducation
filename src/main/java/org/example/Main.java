28f2563e80fc9fc16ddc9091fd67044e0f164778
Alexsand74 <1974alexsand@gmail.com>
2023-01-03 01:19:52 +0300

    The code is in the ! Код находится в ветке !

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+
         System.out.println("The code is in the !");
         System.out.println("Код не находится здесь !!!");
-        System.out.println("Еще код не находится здесь !!!");
+        System.out.println("Код  находится в ветке !!!");
+        System.out.println("The code is in a branch !");
 
     }
 }
